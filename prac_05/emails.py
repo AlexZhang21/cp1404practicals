60de88f4eafb3a489bd068f5226cfd43e5efa0d4
AlexZhang21 <haolun.zhang@my.jc.edu.au>
2023-12-04 13:54:42 +0800

    practical 5

---
+++ prac_05/emails.py
@@ -0,0 +1,8 @@
+def __init__(self, brand="", price=0.0, colour""， speed=1):
+def __str__(self):
+    self.brand = brand
+    self.price = price
+    self.speed = speed
+
+def get self
+    return self.speed +1
